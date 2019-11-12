5b617195309acc92b6bca84dcf5394306d97d31c
Alisson Cavalcanti <alissoncavalcanticma@gmail.com>
2019-11-12 15:35:14 -0300

    Adicionado campo de pesquisa na tela de resumos, ainda sem função

--- view/resumos.php
+++ view/resumos.php
@@ -35,15 +35,29 @@
           <div class="section__content section__content--p30">
               <div class="container-fluid">
                   <div class="row">
-                      <div class="col-md-12" style="line-height:35px; margin-top:30px">
+                        <div class="col-md-12" style="line-height:35px; margin-top:30px">
                           <div style="float: left;">
                               Resumo's
                           </div>
-
-                          <div style="float: right; font-size: 2px">
-                              <button style="font-size: 14px" class="btn btn-success" onclick="window.open('cad_resumo.php', '_self')"> <i class="fa fa-plus"></i> ADD</button>
-                          </div>
-                      </div>
+                          
+                          <div style="float: right">
+                                    <div style="float: right;">
+                                        <div style="float:left">
+                                        <form name="form_search" id="form_search" method="post" action="">
+                                                <fieldset>
+                                                        <div class="input-prepend">
+                                                            <span class="add-on"><i class="icon-search"></i></span>
+                                                            <input type="text" name="search" id="search" tabindex="1" placeholder="  Pesquisar..."/>
+                                                        </div>
+                                                </fieldset>
+                                        </form>
+                                        </div>
+                                        <div style="float:right">
+                                            <button style="font-size: 14px" class="btn btn-success" onclick="window.open('cad_resumo.php', '_self')"> <i class="fa fa-plus"></i> ADD</button>
+                                        </div>
+                                    </div>
+                            </div>
+                        </div>
                   </div>
                   <div class="row m-t-30" style="margin-top: 10px;">
                       <div class="col-md-12">
