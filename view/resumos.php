1821aaad1db5146ceabd8347c79a1f4a454ce2dc
Alisson Cavalcanti <alissoncavalcanticma@gmail.com>
2019-11-13 16:15:32 -0300

    otimizada para melhoramento da leitura de código

--- view/resumos.php
+++ view/resumos.php
@@ -129,34 +129,6 @@
 
                       </div>
                   </div>
-                  <script>
-                      $(document).ready(function() {
-                          $('#listarChecklists').DataTable({
-                              "order": [
-                                  [0, "desc"]
-                              ], //or asc 
-                              "columnDefs": [{
-                                  "targets": 3,
-                                  "type": "date-eu"
-                              }],
-                              "oLanguage": {
-                                  "sProcessing": "Aguarde enquanto os dados são carregados ...",
-                                  "sLengthMenu": "Mostrar _MENU_ registros por pagina",
-                                  "sZeroRecords": "Nenhum registro correspondente ao criterio encontrado",
-                                  "sInfoEmtpy": "Exibindo 0 a 0 de 0 registros",
-                                  "sInfo": "Exibindo de _START_ a _END_ de _TOTAL_ registros",
-                                  "sInfoFiltered": "",
-                                  "sSearch": "Pesquisar: ",
-                                  "oPaginate": {
-                                      "sFirst": "Primeiro",
-                                      "sPrevious": "Anterior",
-                                      "sNext": "Próximo",
-                                      "sLast": "Último"
-                                  }
-                              }
-                          });
-                      });
-                  </script>
                   <!-- RODAPÉ -->
                   <?php include_once 'rodape.php'; ?>
                   <!-- RODAPÉ -->
